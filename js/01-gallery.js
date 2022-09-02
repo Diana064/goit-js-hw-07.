07fc88c849cfb83c82b0371a342be3d148af961a
Diana064 <dipustolyakova02@gmail.com>
2022-09-02 14:53:54 +0200

    add 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = galleryItems;
+const galleryMarkup = document.querySelector('.gallery');
+
+// console.log(gallery);
+// console.log(gallery);
+
+const markup = gallery
+	.map(({ preview, original, description }) => {
+		return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+	})
+	.join('');
+
+galleryMarkup.insertAdjacentHTML('beforeend', markup);
+
+galleryMarkup.addEventListener('click', urlImage);
+
+function urlImage(event) {
+	event.preventDefault();
+	const url = event.target.dataset.source;
+	console.log(url);
+	if (!url) return;
+	// console.log(url);
+	const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`);
+	instance.show(() => console.log('lighboxvisible'));
+}
